4e6f8fd16f66b2692098eb1f1fe29ceb919adca5
ana.camacho <129320167+anacamacho-cloud@users.noreply.github.com>
2024-04-30 11:33:35 -0300

    Add files via upload

---
+++ objeto.js
@@ -0,0 +1,23 @@
+class Obj {
+    constructor (string1, string2){
+         this.string1 = string1 //variavel de interface
+         this.string2 = string2
+    }
+
+    metodo1(){
+        window.open(this.site);
+    }
+
+    metodo2(){
+        
+    }
+}
+
+names = ['Ana','Duda','Berenice',]
+
+
+class Calebe {
+    constructor (string){
+           this.name = string
+    }
+}
